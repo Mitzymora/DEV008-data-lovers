7bf1252693cc7519af26f8ede096afe4d6c03908
Cesar Cipher <danielakastrejon@gmail.com>
2023-05-31 21:54:16 -0600

    Otra prueba que me pidio

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import { example } from './data.js';
 import data from './data/got/got.js';
 // import data from './data/got/got.js';
 
+
 console.log(example, data);
 
-document.getElementById("Buscar").addEventListener("click", function(){
-
-})
