b71336a72e79f5ec91ae8f019ce608fd2be4c059
Cesar Cipher <danielakastrejon@gmail.com>
2023-05-31 21:55:11 -0600

    Otra prueba

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import { example } from './data.js';
 import data from './data/got/got.js';
 // import data from './data/got/got.js';
 
+import data from './data/got/got.js'
+
 
 console.log(example, data);
 
