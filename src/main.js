6ef4cb8b45efe869b32b694a21d29554550e3d66
Mitzymora <mitzamora30@gmail.com>
2023-06-01 11:32:29 -0500

    descartar

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import { filterData, sortData, computeStatus, filterCasas, filterEstatus, searchNombre } from './data.js';
+//import { filterData, sortData, computeStatus, filterCasas, filterEstatus, searchNombre } from './data.js';
 // import data from './data/got/got.js';
 import data from './data/got/got.js';
+//const seleccionar= document.getElementById("ordenalfabetico");
 
-const seleccionar= document.getElementById("ordenalfabetico");
 export const got = [data.personajes]
 console.log(example, data);
 
